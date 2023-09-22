79ffc0a9f02f5129e129b85496949dd7c783f6a2
Niklas Bentzen <niklas@bentzen.it>
2023-09-22 16:21:57 +0200

    Added w1 assignment folder

---
+++ weekly-assignment/w1/Main.java
@@ -0,0 +1,8 @@
+//In this assignment, you must write the famous Hello World program.
+//It must print the text "Hello, World!" exactly and nothing else.
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
